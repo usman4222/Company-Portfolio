8df030aa8952ee0c96194300ca963b54f22c1e0c
Rana Usman <usman4243ch@gmail.com>
2024-04-28 12:50:31 -0700

    Add Project Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './assets/css/styles.css';
 import LandingPage from './pages/LandingPage';
 import DiscussProjectPage from './pages/DiscussProjectPage';
 import ProjectDetailPage from './pages/ProjectDetailPage';
+import ProjectPage from './pages/ProjectPage';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<LandingPage />} />
         <Route path='/discuss-project' element={<DiscussProjectPage />} />
         <Route path='/project/:id' element={<ProjectDetailPage />} />
+        <Route path='/project' element={<ProjectPage />} />
       </Routes>
     </BrowserRouter>
   );
