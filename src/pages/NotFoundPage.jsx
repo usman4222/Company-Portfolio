4fa5233098ccfddd92c6611335c590695d852993
Rana Usman <usman4243ch@gmail.com>
2024-05-01 14:27:43 -0700

    New changes

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -9,7 +9,8 @@ const NotFoundPage = () => {
         <img src={NotFound} alt="Not Found" className="sm:w-3/4 xl:w-5/12" />
       </div>
       <h1 className="text-white text-2xl text-center mt-5">
-        You weren't supposed to find this place...
+        {/* You weren't supposed to find this place... */}
+        Looks like you are lost
       </h1>
       <div className="flex justify-center">
         <Button href="/" type="link" className="flex w-30 h-10 px-5 mt-5 bg-gray-600 text-white items-center rounded transform transition duration-500 hover:bg-gray-900">
