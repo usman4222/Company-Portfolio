dd12e94ac0f9471028ab2eb12b3afdac0661e78b
Rana Usman <usman4243ch@gmail.com>
2024-05-01 09:31:23 -0700

    New Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './pages/andingPage'
+import LandingPage from './pages/LandingPage'
+
 
 
 const App = () => {
